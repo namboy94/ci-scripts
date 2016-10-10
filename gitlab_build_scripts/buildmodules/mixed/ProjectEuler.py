7cc0d3f68d6bfa9636de7f9df35f49d587a99f8b
Hermann Krumrey <hermann@krumreyh.com>
2016-10-10 17:16:22 +0200

    Created basic project euler build flow

--- gitlab_build_scripts/buildmodules/mixed/ProjectEuler.py
+++ gitlab_build_scripts/buildmodules/mixed/ProjectEuler.py
@@ -24,8 +24,13 @@ LICENSE
 
 # imports
 import os
+import json
 from subprocess import Popen
 from typing import List, Dict
+from gitlab_build_scripts.parameters.mixed.ProjectEulerLanguages import Language
+
+# JSON Format:
+# [{language: str, runtime: float, answer: str}, ...]
 
 
 class ProjectEuler(object):
@@ -34,11 +39,51 @@ class ProjectEuler(object):
     """
 
     @staticmethod
-    def build(refresh: bool = True) -> None:
+    def build(languages: List[Language], refresh: bool = True) -> None:
         """
         Starts the build process for the Project Euler readmes
+        This method runs all programs to check
 
-        :param refresh: Flag that can be set to rebuild all problems. If False, will only build new ones
-        :return:        None
+        :param languages: The languages to build
+        :param refresh:   Flag that can be set to rebuild all problems. If False, will only build new ones
+        :return:          None
         """
+        problem_states = {}
+
+        for problem in os.listdir("problems"):
+            json_file_path = os.path.join("problems", problem, "state.json")
+            if not os.path.isfile(json_file_path) or refresh:
+                problem_states[problem] = {}
+            else:
+                with open(json_file_path, 'r') as json_file:
+                    json_content = json.load(json_file)
+                    problem_states[problem] = json_content
+
+        for problem in problem_states:
+
+            problem_state = problem_states[problem]
+            problem_number = int(problem.split("-")[1])
+
+            for language in languages:
+
+                if not language.get_name() in problem_states:
+
+                    # noinspection PyCallingNonCallable
+                    runtime, result = language().run(problem_number)
+
+                    problem_state[language.get_name] = {"runtime": runtime,
+                                                                  "result": result}
+
+            json_file_path = os.path.join("problems", problem, "state.json")
+            with open(json_file_path, 'w') as json_file:
+                json.dump(problem_state, json_file)
+
+        ProjectEuler.generate_readmes(problem_states)
+
+
+
+
+
+
+
 
